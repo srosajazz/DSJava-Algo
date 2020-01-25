81f3a373a38b27f51729a6ad9968a86d8708a2f7
Sergio Rosa <scarlosjazz@gmail.com>
2020-01-25 09:12:01 -0500

    Arrays in Memory

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         int[] intArray = new int[7];
+
+//         intArray[0] = 1;
+//         intArray[1] = 2;
+//         intArray[2] = 3;
+//         intArray[3] = -4;
+//         intArray[4] = 5;
+//         intArray[5] = 6;
+//         intArray[6] = 7;
+
+//         for (int i = 0; i < intArray.length; i++) {
+//             System.out.println(intArray[i]);
+//         }
+//     }
+// }
+
+//2
 public class Main {
     public static void main(String[] args) {
         int[] intArray = new int[7];
@@ -10,8 +29,15 @@ public class Main {
         intArray[5] = 6;
         intArray[6] = 7;
 
+        int index = -1; // if dont find the array return -1
         for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
+            if (intArray[i] == -4) {
+                index = i;
+                break;
+            }
+
         }
+
+        System.out.println("index = " + index);
     }
 }
